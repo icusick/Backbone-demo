2ce2848b34c4a1f2658fff442730448cce208552
Nathan Riemer <nathan.riemer@gmail.com>
2016-01-19 15:17:31 -0500

    Made it a table still fighting with sorting

--- public/main.js
+++ public/main.js
@@ -7,11 +7,38 @@ var Dog = Backbone.Model.extend({
 var DogCollection = Backbone.Collection.extend({
   url: '/dogs',
 
-  model: Dog
+  model: Dog,
+
+  // sortByColumn: function(colName) {
+  //   this.sortKey = colName;
+  //   this.sort();
+  // }
+  sortAttribute: "energy",
+  sortDirection: 1,
+ 
+  sortDogs: function (attr) {
+      this.sortAttribute = attr;
+      this.sort();
+   },
+ 
+   comparator: function(a, b) {
+      var a = a.get(this.sortAttribute),
+          b = b.get(this.sortAttribute);
+ 
+      if (a == b) return 0;
+ 
+      if (this.sortDirection == 1) {
+         return a > b ? 1 : -1;
+      } else {
+         return a < b ? 1 : -1;
+      }
+   }
+
+
 })
 
 var DogView = Backbone.View.extend({
-  tagName: "li",
+  tagName: "tr",
   //our dog views listen for two events on their models
   //if a model 'changes', the view is re rendered
   //if a model 'destroys', the view is removed
@@ -24,7 +51,16 @@ var DogView = Backbone.View.extend({
     'click #show-edit': 'edit',
     'click #delete': 'delete',
     'click #update': 'update'
+    // 'click': 'sortThingsByColumn'
   },
+
+  // sortThingsByColumn: function(event) {
+  //   console.log('grr');
+  //   console.log(event.target);
+  //   var column = event.currentTarget.classList[0]
+  //   this.sortByColumn(column)
+  //   this.render()
+  // },
 
   template: $("#message-template").html(),
   editTemplate: $("#edit-form").html(),
@@ -47,11 +83,18 @@ var DogView = Backbone.View.extend({
     //this function isn't ideal, because it allows a user to reset the model with empty attributes
     var nameUpdate = $("[name='edit-name']").val();
     var breedUpdate = $("[name='edit-breed']").val();
-    this.model.set({name: nameUpdate, breed: breedUpdate});
+    var cutenessUpdate = $("[name='edit-cuteness']").val();
+    var behaviorUpdate = $("[name='edit-behavior']").val();
+    var energyUpdate = $("[name='edit-energy']").val();
+
+    this.model.set({name: nameUpdate, breed: breedUpdate, cuteness: cutenessUpdate, behavior: behaviorUpdate, energy: energyUpdate});
     this.model.save();
     //this simply resets the input values to be empty after the model is updated
     nameUpdate.val("");
     breedUpdate.val("");
+    cutenessUpdate.val("");
+    behaviorUpdate.val("");
+    energyUpdate.val("");
   },
 
   delete: function(){
@@ -60,22 +103,25 @@ var DogView = Backbone.View.extend({
   }
 })
 
-var DogShow = Backbone.View.extend({
-
-})
-
 var DogCollectionView = Backbone.View.extend({
   initialize: function(){
     //set behavior that fires on the creation of every instance
     //listen to my collection i'm observing, if somethings added, do something
     this.listenTo(this.collection, 'add', this.addDog);
+    this.listenTo(this.collection, 'sort', this.updateTable);
+
   },
 
   addDog: function(modelFromCollection){
     var newDogView = new DogView({model: modelFromCollection});
     newDogView.render();
-    this.$el.append(newDogView.$el)
+    this.$el.append(newDogView.$el);
+  },
+
+  updateTable: function() {
+    this.collection.fetch();
   }
+
 })
 
 var FormView = Backbone.View.extend({
@@ -92,12 +138,20 @@ var FormView = Backbone.View.extend({
     event.preventDefault()
     var name = $("[name='name']").val();
     var breed = $("[name='breed']").val();
+    var cuteness = $("[name='cuteness']").val();
+    var behavior = $("[name='behavior']").val();
+    var energy = $("[name='energy']").val();
 
 
-    var newModel = new Dog({name: name, breed: breed})
+
+    var newModel = new Dog({name: name, breed: breed, cuteness: cuteness, behavior: behavior, energy: energy})
     this.dogCollection.create(newModel)
     $("[name='name']").val("");
     $("[name='breed']").val("");
+    $("[name='cuteness']").val("");
+    $("[name='behavior']").val("");
+    $("[name='energy']").val("");
+
   }
 })
 
@@ -110,10 +164,12 @@ var Router = Backbone.Router.extend({
 
   index: function(){
     var dogCollection = new DogCollection()
+    dogCollection.sortDirection = -1;
+    dogCollection.sortDogs('energy');
     //when we create these views, we pass in a 'live' piece of the DOM as the top level el.
-    var dogCollectionView = new DogCollectionView({collection: dogCollection, el: $("ul")})
+    var dogCollectionView = new DogCollectionView({collection: dogCollection, el: $("table")})
     var formView = new FormView({el: $("form"), collection: dogCollection})
-    dogCollection.fetch()
+    dogCollection.fetch();
   },
 
   show: function(id){
